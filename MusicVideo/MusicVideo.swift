1af07838a077d756b3b6b69b345db1cb554e0669
Manuel Piquero <mpiquero@hvc.rr.com>
2016-03-13 15:16:02 -0400

    completed part 4

--- MusicVideo/MusicVideo.swift
+++ MusicVideo/MusicVideo.swift
@@ -12,8 +12,15 @@ class Videos {
     //Data Encapsulation
     
     private var _vName: String
+    private var _vRights: String
+    private var _vPrice: String
     private var _vImageUrl: String
+    private var _vArtist: String
     private var _vVideoUrl: String
+    private var _vImid: String
+    private var _vGenre: String
+    private var _vLinkToiTunes: String
+    private var _vReleaseDte: String
     
     //Make a getter
     
@@ -21,16 +28,44 @@ class Videos {
         return _vName
     }
     
+    var vRights: String {
+        return _vRights
+    }
+    
+    var vPrice: String {
+        return _vPrice
+    }
+    
     var vImageUrl: String {
         return _vImageUrl
+    }
+    
+    var vArtist: String {
+        return _vArtist
     }
     
     var vVideoUrl: String {
         return _vVideoUrl
     }
     
+    var vImid: String {
+        return _vImid
+    }
+    
+    var vGenre: String {
+        return _vGenre
+    }
+    
+    var vLinkToiTunes: String {
+        return _vLinkToiTunes
+    }
+    
+    var vReleasDte: String {
+        return _vReleaseDte
+    }
+    
     init(data: JSONDictionary) {
-        //Video Name
+        ///Video Name
         if let name = data["im:name"] as? JSONDictionary, vName = name["label"] as? String {
             self._vName = vName
         }
@@ -39,7 +74,25 @@ class Videos {
             _vName = ""
         }
         
-        // the video Image
+        ///Video rights
+        if let rights = data["rights"] as? JSONDictionary, vRights = rights["label"] as? String {
+            self._vRights = vRights
+        }
+        else {
+            // you may not always get data back from the JSON  you may want to display a message
+            _vRights = ""
+        }
+        
+        ///Video Artist
+        if let artist = data["im:artist"] as? JSONDictionary, vArtist = artist["label"] as? String {
+            self._vArtist = vArtist
+        }
+        else {
+            // you may not always get data back from the JSON  you may want to display a message
+            _vArtist = ""
+        }
+        
+        /// the video Image
         if let img = data["im:image"] as? JSONArray,
             image = img[2] as? JSONDictionary,
             immage = image["label"] as? String {
@@ -49,7 +102,7 @@ class Videos {
             _vImageUrl = ""
         }
         
-        // the video
+        /// the video
         if let video = data["link"] as? JSONArray,
             vUrl = video[1] as? JSONDictionary,
             vHref = vUrl["attributes"] as? JSONDictionary,
@@ -60,5 +113,56 @@ class Videos {
             _vVideoUrl = ""
         }
         
+        ///Video id
+        if let content = data["id"] as? JSONDictionary,
+            vAtrib = content["attributes"] as? JSONDictionary ,
+            vImid = vAtrib["im:id"] as? String {
+                self._vImid = vImid
+        }
+        else {
+            // you may not always get data back from the JSON  you may want to display a message
+            _vImid = ""
+        }
+        
+        ///Video genre
+        if let genre = data["category"] as? JSONDictionary,
+            vAtrib = genre["attributes"] as? JSONDictionary ,
+            vGenre = vAtrib["label"] as? String {
+            self._vGenre = vGenre
+        }
+        else {
+            // you may not always get data back from the JSON  you may want to display a message
+            _vGenre = ""
+        }
+        
+        ///Video price
+        if let price = data["im:price"] as? JSONDictionary, vPrice = price["label"] as? String {
+            self._vPrice = vPrice
+        }
+        else {
+            // you may not always get data back from the JSON  you may want to display a message
+            _vPrice = ""
+        }
+        
+        ///Video link to itunes
+        if let link = data["id"] as? JSONDictionary,
+            vLinkToiTunes = link["label"] as? String {
+                self._vLinkToiTunes = vLinkToiTunes
+        }
+        else {
+            // you may not always get data back from the JSON  you may want to display a message
+            _vLinkToiTunes = ""
+        }
+        
+        ///Video release date
+        if let date = data["im:releaseDate"] as? JSONDictionary,
+            vAtrib = date["attributes"] as? JSONDictionary ,
+            vReleasDte = vAtrib["label"] as? String {
+                self._vReleaseDte = vReleasDte
+        }
+        else {
+            // you may not always get data back from the JSON  you may want to display a message
+            _vReleaseDte = ""
+        }
     }
 }
